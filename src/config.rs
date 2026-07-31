e5e6dc666f3f2d69dd0c53e6e694485e39222f6a
huiinyg-rusting <huiinyg@outlook.com>
2026-07-31 09:58:02 +0800

    update config system

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::thread;
 
 pub fn init() {
     let path = match exe_config_path() {
@@ -7,32 +8,33 @@ pub fn init() {
         None => return,
     };
     if !path.exists() {
-        let _ = fs::write(&path, "[routes]\ndisabled = []\n");
+        let _ = fs::write(&path,  format!("[routes]\ndisabled = []\nnumofcore = {}", realcorenum()));
     }
 }
 
-pub fn is_route_disabled(route: &str) -> bool {
-    let path = match exe_config_path() {
-        Some(p) => p,
-        None => return false,
-    };
-    let content = match fs::read_to_string(&path) {
-        Ok(c) => c,
-        Err(_) => return false,
-    };
-    let config: Config = match toml::from_str(&content) {
-        Ok(c) => c,
-        Err(_) => return false,
-    };
+fn load_config() -> Config {
+    let path = exe_config_path().expect("Can't find Config.toml");
+    let content = fs::read_to_string(&path).expect("Can't read Config.toml");
+    let config: Config = toml::from_str(&content).expect("Can't Toml the config string");
     config
-        .routes
-        .and_then(|r| r.disabled)
-        .map_or(false, |v| v.contains(&route.to_string()))
 }
 
 fn exe_config_path() -> Option<std::path::PathBuf> {
     let exe = env::current_exe().ok()?;
     Some(exe.parent()?.join("config.toml"))
+}
+
+fn realcorenum() -> u8 {
+    match thread::available_parallelism() {
+        Ok(threads) => {
+            threads.get().try_into().unwrap_or(u8::MAX)
+        }
+        Err(_) => {
+            log::error!("‌Failed to retrieve thread count; defaulting to 1.");
+            1
+        }, 
+
+    }
 }
 
 #[derive(serde::Deserialize)]
@@ -43,4 +45,21 @@ struct Config {
 #[derive(serde::Deserialize)]
 struct RoutesConfig {
     disabled: Option<Vec<String>>,
+    numofcore: Option<u8>,
 }
+
+pub fn is_route_disabled(route: &str) -> bool {
+    load_config()
+        .routes
+        .and_then(|r: RoutesConfig| r.disabled)
+        .map_or(false, |v| v.contains(&route.to_string()))
+}
+
+pub fn numofcore() -> u8 {
+    load_config()
+        .routes
+        .and_then(|r: RoutesConfig| r.numofcore)
+        .unwrap_or_else(|| realcorenum())
+
+}
+
